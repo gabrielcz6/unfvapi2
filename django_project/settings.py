f674d4e195a3eaf4c15e80e2afffafd9d999aaea
Gabriel <gabrielcanepamercado@gmail.com>
2023-08-03 23:01:12 -0500

    Tu mensaje de commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -119,9 +119,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
         "default": env.dj_db_url("DATABASE_URL") # new
-    }
 }
 
 
