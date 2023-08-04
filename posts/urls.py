4a8f4d374ac989a04c121260943c9814cf0957ea
Gabriel <gabrielcanepamercado@gmail.com>
2023-08-03 22:17:32 -0500

    add environment variables

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,7 @@
 # posts/urls.py
 from django.urls import path
 from .views import PostList, PostDetail, UserList, UserDetail
-
+from drf_spectacular.views import (SpectacularAPIView,SpectacularRedocView) # new
 
 
 urlpatterns = [
@@ -9,4 +9,5 @@ path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
 path("", PostList.as_view(), name="post_list"),
 path("users/", UserList.as_view()), # new
 path("users/<int:pk>/", UserDetail.as_view()), # new
+
 ]
